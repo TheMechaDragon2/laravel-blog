9c8b8b202f70e3aa35ea6e10ceab18d753fd70e5
TheMechaDragon2 <themechadragon49@gmail.com>
2025-07-01 11:54:36 +0300

    laravel 2

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,16 +2,47 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class CommentSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $posts = Post::where('is_published', true)->get();
+        $users = User::all();
+
+        foreach ($posts as $post) {
+            // Create parent comments
+            $parentComments = Comment::factory(rand(2, 5))
+                ->approved()
+                ->create([
+                    'post_id' => $post->id,
+                    'user_id' => $users->random()->id,
+                ]);
+
+            // Create replies to some parent comments
+            foreach ($parentComments as $parentComment) {
+                if (rand(0, 100) < 40) { // 40% chance of having replies
+                    Comment::factory(rand(1, 3))
+                        ->approved()
+                        ->create([
+                            'post_id' => $post->id,
+                            'user_id' => $users->random()->id,
+                            'parent_id' => $parentComment->id,
+                        ]);
+                }
+            }
+
+            // Create some pending comments
+            Comment::factory(rand(0, 2))
+                ->pending()
+                ->create([
+                    'post_id' => $post->id,
+                    'user_id' => $users->random()->id,
+                ]);
+        }
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,58 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $users = User::all();
+
+        // Create published posts
+        Post::factory(15)
+            ->published()
+            ->recycle($users)
+            ->create();
+
+        // Create draft posts
+        Post::factory(5)
+            ->draft()
+            ->recycle($users)
+            ->create();
+
+        // Create specific featured post
+        Post::create([
+            'title' => 'Welcome to Our Laravel Blog Tutorial',
+            'slug' => 'welcome-to-our-laravel-blog-tutorial',
+            'excerpt' => 'This is the first post in our Laravel blog tutorial series. Learn how to build a complete blog application with Laravel and Livewire.',
+            'content' => '
+                <h2>Welcome to the Laravel Blog Tutorial</h2>
+                <p>This tutorial series will guide you through building a complete blog application using Laravel 11 and Livewire 3.</p>
+
+                <h3>What You\'ll Learn</h3>
+                <ul>
+                    <li>Setting up Laravel with Docker</li>
+                    <li>Implementing authentication with Sanctum</li>
+                    <li>Creating database relationships</li>
+                    <li>Building interactive components with Livewire</li>
+                    <li>Styling with Tailwind CSS</li>
+                </ul>
+
+                <h3>Prerequisites</h3>
+                <p>Basic knowledge of PHP and Laravel is recommended but not required. We\'ll guide you through each step.</p>
+
+                <h3>Getting Started</h3>
+                <p>Follow along with the daily tutorials and build your own blog application. Each day builds upon the previous lessons.</p>
+
+                <p>Happy coding! ðŸš€</p>
+            ',
+            'featured_image' => 'https://picsum.photos/800/400?random=1',
+            'is_published' => true,
+            'published_at' => now(),
+            'user_id' => User::where('email', 'admin@admin.com')->first()->id,
+        ]);
     }
 }
